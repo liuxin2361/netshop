c1c21d46bd6443e4bee457a2a35898d389aa4d25
Xin Liu <liuxin02030601@163.com>
2020-03-09 17:00:54 +0800

    用户登录和登出功能

--- userapp/urls.py
+++ userapp/urls.py
@@ -7,4 +7,8 @@ app_name = 'userapp'
 urlpatterns = [
     path('register/', views.RegisterView.as_view()),
     path('center/', views.center_view),
+    path('login/', views.LoginView.as_view()),
+    path('loadCode/', views.LoadCodeView.as_view()),
+    path('checkCode/', views.CheckCodeView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -1,10 +1,12 @@
 import jsonpickle
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
 
 from userapp.models import UserInfo
+from utils.code import gene_code
 
 
 class RegisterView(View):
@@ -28,3 +30,51 @@ class RegisterView(View):
 
 def center_view(request):
     return render(request, 'center.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        # 获取请求参数
+        uname = request.POST.get('account', '')
+        pwd = request.POST.get('password', '')
+
+        # 判断是否登录成功
+        user = UserInfo.objects.filter(uname=uname, pwd=pwd)
+        if user:
+            request.session['user'] = jsonpickle.dumps(user[0])
+            return redirect('/user/center/')
+
+        return redirect('/user/login/')
+
+
+class LoadCodeView(View):
+    def get(self, request):
+        # 获取图片验证码
+        img, code = gene_code()
+        request.session['session_code'] = code
+        return HttpResponse(img, content_type='image/png')  # content_type指定图片的渲染格式
+
+
+class CheckCodeView(View):
+    def get(self, request):
+        # 获取请求参数
+        code = request.GET.get('code', -1)
+        # 获取session中生成的验证码
+        session_code = request.session.get('session_code', '')
+        # 判断是否相等
+        vflag = False
+        if code == session_code:
+            vflag = True
+        # 返回响应
+        return JsonResponse({'vflag': vflag})
+
+
+class LogoutView(View):
+    def post(self, request):
+        # 清空session中所有数据
+        request.session.flush()
+        # 返回响应
+        return JsonResponse({'logout': True})
